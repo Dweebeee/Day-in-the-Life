03404014e36dab2163eb605c6892840a22b22e7b
Deeb <108564003+Dweebeee@users.noreply.github.com>
2023-07-09 20:35:46 +1000

    Endless Wave Mode
    Added the (probably) endless mode.

--- Assets/Scripts/CombatWaveManager.cs
+++ Assets/Scripts/CombatWaveManager.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 
 public class CombatWaveManager : MonoBehaviour
 {
-    public List<EnemyAI> knightsAI;
-    int wavesPassed = 0;
-    public List<int> waveEnemies;
-    int enemiesKilled = 0;
+    public List<EnemyAI> knightsAI; // TODO remove this.
+    int wavesPassed = 0; // Current number of waves completed. Incremented when the last enemy in a wave is killed.
+    int enemiesKilled = 0; // Number of enemies killed in the current wave. Reset to 0 when new wave is started.
+    public List<int> waveEnemies; // List of the number of enemies to spawn in each wave.
+    public List<Vector2> enemySpawns; // List of possible places for the enemies to spawn.
+    public List<GameObject> currentEnemies; // All the current enemies that have been created in the current wave.
+    public GameObject knightPrefab; // The prefab of the knight to create.
 
     public void enemyKilled()
     {
@@ -22,18 +25,45 @@ public class CombatWaveManager : MonoBehaviour
 
     public void incrementWave()
     {
+        // Change wave numbers.
         wavesPassed++;
         enemiesKilled = 0;
-        Debug.Log("Starting next wave");
-        startNextWave();
+
+        // Delete any remaining objects in the currentEnemies list if they haven't already been destroyed.
+        for (int i = 0; i < currentEnemies.Count; i++)
+        {
+            Destroy(currentEnemies[i]);
+        }
+
+        // If we are not at the last wave listed, move on to the next wave.
+        if (wavesPassed <= waveEnemies.count)
+        {
+            Debug.Log("Starting next wave");
+            SpawnWaveEnemies();
+        }
     }
 
-    private void startNextWave()
+    private void SpawnWaveEnemies()
     {
+        // Set up a random class to all random knight spawn.
+        Random rnd = new Random();
+
         for (int i = 0; i < waveEnemies[wavesPassed]; i++)
         {
-            Debug.Log("Wave " + Convert.ToString(wavesPassed) + " knight " + Convert.ToString(i) + " Spawned");
+            Debug.Log("Wave " + Convert.ToString(wavesPassed) + " knight " + Convert.ToString(i) + " spawned");
+            //Debug.Log($"Wave {wavesPassed} knight {i} spawned.");
+
             knightsAI[i].combat = true;
+
+            // Get a spawn location.
+            int randomSpawnIndex = rnd.Next(0, 7);
+
+            // Create a new enemy.
+            currentEnemies.Add(Instantiate(knightPrefab, enemySpawns[randomSpawnIndex]));
+
+            // Need to enable AI? If so:
+            //currentEnemies[i].combat = true;
+
         }
     }
 }
